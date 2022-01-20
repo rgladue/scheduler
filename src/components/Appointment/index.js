056fc916b338ad0b4779b6c635a031ab97e75fc8
Robert <robgladue@hotmail.com>
2022-01-19 18:56:26 -0600

    add appointment folder with components

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "components/Appointment/styles.scss";
+
+const Appointment = function (props) {
+  return (
+    <article className="appointment"></article>
+  )
+}
+
+export default Appointment;
